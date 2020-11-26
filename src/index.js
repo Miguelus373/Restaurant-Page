3e5dd817d7d8b7b6d7aca2bc127d006539d91a68
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-11-26 12:44:41 -0400

    Add main page content and load module

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import main from './main_load';
+
+const content = document.getElementById('content');
+
+main(content);
---
+++ src/main_load.js
@@ -0,0 +1,8 @@
+const mainLoad = (content) => {
+  content.innerHTML = `<div class='main-page'>
+  <h1 class='headline'>Cake Shop</h1>
+  <p class='description'>Get the best cakes of the city in this amazing cake shop</p>
+  </div>`;
+};
+
+export { mainLoad as default };
